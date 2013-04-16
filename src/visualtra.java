bcd2b5601954910aba5f055396da824bab4d6a20
alex dobrianski <adobri@shaw.ca>
2013-04-16 02:20:40 -0700

    proper texture & light

--- src/visualtra.java
+++ src/visualtra.java
@@ -18,6 +18,7 @@ import org.w3c.dom.Element;
 import java.io.File;  
 import java.io.InputStream;  
 import java.net.*;
+import com.sun.j3d.utils.behaviors.mouse.*;
   
 
 public class visualtra extends Applet {
@@ -25,15 +26,19 @@ public class visualtra extends Applet {
     private float EarthX;
     private float EarthY;
     private float EarthZ;
+    private float EarthR;
     Texture texEarth;
     
     private float MoonX;
     private float MoonY;
     private float MoonZ;
+    private float MoonR;
+    Texture texMoon;
     
     private float SunX;
     private float SunY;
     private float SunZ;
+    private float SunR;
     
     // Constants for type of light to use
     private static final int DIRECTIONAL_LIGHT = 0;
@@ -46,15 +51,12 @@ public class visualtra extends Applet {
 
     private SimpleUniverse u = null;
 
-    public BranchGroup createSceneGraph(SimpleUniverse u) {
-	Color3f eColor    = new Color3f(0.0f, 0.0f, 0.0f);
-	Color3f sColor    = new Color3f(0.0f, 0.0f, 0.0f);
-	Color3f objColor  = new Color3f(.6f, .6f, .6f);
-        
-	Color3f lColor1   = new Color3f(1.0f, 1.0f, 1.0f);
+    public BranchGroup createSceneGraph(SimpleUniverse u) 
+    {
+
 	Color3f lColor2   = new Color3f(0.0f, 0.0f, 1.0f);
 
-        Color3f alColor   = new Color3f(0.2f, 0.2f, 0.2f);
+        Color3f alColor   = new Color3f(.2f, .2f, .2f);
 	Color3f bgColor   = new Color3f(0.05f, 0.05f, 0.2f);
 
 	Transform3D t;
@@ -67,45 +69,66 @@ public class visualtra extends Applet {
         TransformGroup objScale = new TransformGroup();
         Transform3D t3d = new Transform3D();
         //Vector3f vector = new Vector3f(-.2f,.1f , -.4f);
-        t3d.setScale(0.4);
+        t3d.setScale(0.6);
         //t3d.setTranslation(vector);
         objScale.setTransform(t3d);
         objRoot.addChild(objScale);
 
 	// Create a bounds for the background and lights
 	BoundingSphere bounds =
-	    new BoundingSphere(new Point3d(0.0,0.0,0.0), 100.0);
+	    new BoundingSphere(new Point3d(0.0,0.0,0.0), 1000.0);
 
 	// Set up the background
 	Background bg = new Background(bgColor);
 	bg.setApplicationBounds(bounds);
 	objScale.addChild(bg);
 
-	// Create a Sphere object, generate one copy of the sphere,
+	// Create a Earth Sphere object, generate one copy of the sphere,
 	// and add it into the scene graph.
-	Material m = new Material(objColor, eColor, objColor, sColor, 100.0f);
+        Color3f eColorEarth    = new Color3f(.0f, .0f, .0f);
+	Color3f sColorEarth    = new Color3f(1.0f, 1.0f, 1.0f);
+	Color3f objColorEarth  = new Color3f(.90f, .90f, .90f);
+	Material m = new Material(objColorEarth, eColorEarth, objColorEarth, sColorEarth, 100.0f);
         //Material m = new Material();
 	Appearance a = new Appearance();
 	m.setLightingEnable(true);
 	a.setMaterial(m);
-        //a.setTexture(texEarth);
+        a.setTexture(texEarth);
         TextureAttributes texAttr = new TextureAttributes();
-        texAttr.setTextureMode(TextureAttributes.BLEND);//MODULATE);
+        texAttr.setTextureMode(TextureAttributes.COMBINE);//.COMBINE);//.BLEND);//.MODULATE);
         a.setTextureAttributes(texAttr);
-	Sphere sph = new Sphere(1.5f, Sphere.GENERATE_NORMALS, 180, a);
+	Sphere sphEarth = new Sphere(EarthR, Sphere.GENERATE_NORMALS | Sphere.GENERATE_TEXTURE_COORDS, 180, a);//.GENERATE_NORMALS, 180, a);
         
-        Transform3D Location = new Transform3D();
-        Vector3f vectorEarth = new Vector3f(EarthX,EarthY , EarthZ);
-        Location.setTranslation(vectorEarth);
-        TransformGroup tg = new TransformGroup(Location);
-        
-        //Location.setTranslation(vector);
-        //tg.setTransform(Location);
-        tg.addChild(sph);
-        //objScale.setTranslation(vector);
-        //t3d.setTranslation(vector);
+        // position of the earth
+        Transform3D LocationEarth = new Transform3D();
+        Vector3f vectorEarth = new Vector3f(EarthX, EarthY , EarthZ);
+        LocationEarth.setTranslation(vectorEarth);
+        TransformGroup tg = new TransformGroup(LocationEarth);
+        tg.addChild(sphEarth);
         objScale.addChild(tg);
 	//objScale.addChild(sph);
+        
+        // Create a Moon Sphere object, generate one copy of the sphere,
+	// and add it into the scene graph.
+        Color3f eColorMoon    = new Color3f(.0f, .0f, .0f);
+	Color3f sColorMoon    = new Color3f(1.0f, 1.0f, 1.0f);
+	Color3f objColorMoon  = new Color3f(.90f, .90f, .90f);
+	Material mMoon = new Material(objColorMoon, eColorMoon, objColorMoon, sColorMoon, 100.0f);
+	Appearance aMoon = new Appearance();
+	//aMoon.setLightingEnable(true);
+	aMoon.setMaterial(mMoon);
+        aMoon.setTexture(texMoon);
+        TextureAttributes texAttrMoon = new TextureAttributes();
+        texAttrMoon.setTextureMode(TextureAttributes.COMBINE);//.COMBINE);//.BLEND);//.MODULATE);
+        aMoon.setTextureAttributes(texAttr);
+	Sphere sphMoon = new Sphere(MoonR, Sphere.GENERATE_NORMALS | Sphere.GENERATE_TEXTURE_COORDS, 180, aMoon);//.GENERATE_NORMALS, 180, a);
+        
+        Transform3D LocationMoon = new Transform3D();
+        Vector3f vectorMoon = new Vector3f(MoonX, MoonY, MoonZ);
+        LocationMoon.setTranslation(vectorMoon);
+        TransformGroup tgMoon = new TransformGroup(LocationMoon);
+        tgMoon.addChild(sphMoon);
+        objScale.addChild(tgMoon);
 
 	// Create the transform group node for the each light and initialize
 	// it to the identity.  Enable the TRANSFORM_WRITE capability so that
@@ -119,44 +142,46 @@ public class visualtra extends Applet {
 	l2RotTrans.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
 	objScale.addChild(l2RotTrans);
 
+        AmbientLight aLgt = new AmbientLight(alColor);
+	Point3f lPoint  = new Point3f(0.0f, 0.0f, 0.0f);
+	Point3f atten = new Point3f(1.0f, 0.0f, 0.0f);
+        
 	// Create transformations for the positional lights
 	t = new Transform3D();
 	Vector3d lPos1 =  new Vector3d(0.0, 0.0, 2.0);
 	t.set(lPos1);
 	TransformGroup l1Trans = new TransformGroup(t);
 	l1RotTrans.addChild(l1Trans);
+        // Create Geometry for point lights
+        Color3f lColor1   = new Color3f(20.0f, 20.0f, 20.0f);
+        ColoringAttributes caL1 = new ColoringAttributes();
+        caL1.setColor(lColor1);
+        Appearance appL1 = new Appearance();
+        appL1.setColoringAttributes(caL1);
+        l1Trans.addChild(new Sphere(0.01f, appL1));
+        // Create lights
+        Light lgt1 = null;
+	Vector3f lDirect1 = new Vector3f(lPos1);
+	lDirect1.negate();
 
+        // Create transformations for the positional lights
 	t = new Transform3D();
 	Vector3d lPos2 = new Vector3d(0.5, 0.8, 2.0);
 	t.set(lPos2);
 	TransformGroup l2Trans = new TransformGroup(t);
 	l2RotTrans.addChild(l2Trans);
-
-	// Create Geometry for point lights
-	ColoringAttributes caL1 = new ColoringAttributes();
-	ColoringAttributes caL2 = new ColoringAttributes();
-	caL1.setColor(lColor1);
+        // Create Geometry for point lights
+        ColoringAttributes caL2 = new ColoringAttributes();
 	caL2.setColor(lColor2);
-	Appearance appL1 = new Appearance();
 	Appearance appL2 = new Appearance();
-	appL1.setColoringAttributes(caL1);
 	appL2.setColoringAttributes(caL2);
-	l1Trans.addChild(new Sphere(0.05f, appL1));
-	l2Trans.addChild(new Sphere(0.05f, appL2));
-
+	l2Trans.addChild(new Sphere(0.01f, appL2));
 	// Create lights
-	AmbientLight aLgt = new AmbientLight(alColor);
-
-	Light lgt1 = null;
 	Light lgt2 = null;
-
-	Point3f lPoint  = new Point3f(0.0f, 0.0f, 0.0f);
-	Point3f atten = new Point3f(1.0f, 0.0f, 0.0f);
-	Vector3f lDirect1 = new Vector3f(lPos1);
 	Vector3f lDirect2 = new Vector3f(lPos2);
-	lDirect1.negate();
 	lDirect2.negate();
 
+        
 	switch (lightType) {
 	case DIRECTIONAL_LIGHT:
 	    lgt1 = new DirectionalLight(lColor1, lDirect1);
@@ -212,7 +237,7 @@ public class visualtra extends Applet {
 				     l2RotTrans,
 				     yAxis,
 				     0.0f, 0.0f);
-	bounds = new BoundingSphere(new Point3d(0.0,0.0,0.0), 100.0);
+	bounds = new BoundingSphere(new Point3d(0.0,0.0,0.0), 1000.0);
 	rotator2.setSchedulingBounds(bounds);
 	l2RotTrans.addChild(rotator2);
 
@@ -235,6 +260,11 @@ public class visualtra extends Applet {
 				     2.0f, 3.5f);
 	translator.setSchedulingBounds(bounds);
 	objScale.addChild(translator);
+        
+        //MouseRotate behavior = new MouseRotate(); 
+        //behavior.setTransformGroup(objRoot); 
+        //objRoot.addChild(behavior); 
+        //behavior.setSchedulingBounds(bounds);
 
         // Let Java 3D perform optimizations on this scene graph.
         objRoot.compile();
@@ -292,25 +322,31 @@ public class visualtra extends Applet {
                     String strZ = ((Node) lstZ.item(0)).getNodeValue();
                     //System.out.println("Z: " + strZ);                    
                     
+                    NodeList lstRElmntLst = fstElmnt.getElementsByTagName("R");
+                    Element lstRElmnt = (Element) lstRElmntLst.item(0);
+                    NodeList lstR = lstRElmnt.getChildNodes();
+                    String strR = ((Node) lstR.item(0)).getNodeValue();
+                    //System.out.println("Z: " + strZ);                    
+                    
                     if (strType.compareTo("Earth") == 0)
                     {
-                        EarthX = Float.valueOf(strX);
-                        EarthY= Float.valueOf(strY);
-                        EarthZ= Float.valueOf(strZ);
+                        EarthX = Float.valueOf(strX); EarthY= Float.valueOf(strY); EarthZ= Float.valueOf(strZ);
+                        EarthR= Float.valueOf(strR);
+                        EarthX /=100000000000.0;EarthY /=100000000000.0;EarthZ /=100000000000.0;EarthR /=10000000.0;
                     }
                     
                     if (strType.compareTo("Moon") == 0)
                     {
-                        MoonX = Float.valueOf(strX);
-                        MoonY= Float.valueOf(strY);
-                        MoonZ= Float.valueOf(strZ);
+                        MoonX = Float.valueOf(strX); MoonY= Float.valueOf(strY); MoonZ= Float.valueOf(strZ);
+                        MoonR= Float.valueOf(strR);
+                        MoonX /=100000000000.0;MoonY /=-100000000000.0;MoonZ /=100000000000.0;MoonR /=10000000.0;
                     }
                     
                     if (strType.compareTo("Sun") == 0)
                     {
-                        SunX = Float.valueOf(strX);
-                        SunY= Float.valueOf(strY);
-                        SunZ= Float.valueOf(strZ);
+                        SunX = Float.valueOf(strX); SunY= Float.valueOf(strY); SunZ= Float.valueOf(strZ);
+                        SunR= Float.valueOf(strR);
+                        SunX /=10000000.0;SunY /=10000000.0;SunZ /=10000000.0;SunR /=10000000.0;
                     }
                 }
             } 
@@ -320,25 +356,79 @@ public class visualtra extends Applet {
             e.printStackTrace();  
         }   
         
+        /*Toolkit tk = Toolkit.getDefaultToolkit();
+   
+        Image imgEarth = tk.getImage("http://24.84.57.253/SatCtrl/Earth-Color_960_Koord.jpg");
+        Image imgMoon = tk.getImage("http://24.84.57.253/SatCtrl/moon___map_by_horizoied-d3y3lvg.jpg");
+   
         try 
         {
-            //TextureLoader texLoader =  new TextureLoader( "SatCtrl/Earth-Color_960_Koord.jpg", "RGBA", this);//new Container());//this );
+            MediaTracker tracker = new MediaTracker(this);
+            tracker.addImage(imgEarth, 0);
+            tracker.addImage(imgMoon, 1);
+            //tracker.waitForID(0);
+            tracker.waitForAll();
+	}
+        catch ( Exception e ) 
+        {
+            e.printStackTrace();  
+        }*/
         
-            //URL urlJpg = new URL("http://24.84.57.253/SatCtrl/Earth-Color_960_Koord.jpg");
+        //ImageComponent2D image; 
+        //texEarth = new Texture2D();
+        //texEarth.setImage(0, image);
+        /*TextureLoader texLoader =  new TextureLoader( "SatCtrl/earthspec1k.jpg", "RGBA", this);//new Container());//this );
+        ImageComponent2D image = texLoader.getImage();
+        int isize = image.getHeight();
+        isize = image.getWidth();
+        //texEarth = new Texture2D();
+        texEarth = new Texture2D(Texture.BASE_LEVEL, Texture.RGBA, image.getWidth(), image.getHeight());
+        try 
+        {
+            texEarth.setImage(0, image);
+        }
+        catch (Exception e) 
+        {  
+            e.printStackTrace();  
+        }*/
+
+        
+        try 
+        {
+            //TextureLoader texLoader =  new TextureLoader( "SatCtrl/Earth-Color_960_Koord.jpg", this);
+            URL ur = new URL("http://24.84.57.253/SatCtrl/Earth-Color_960_Koord.jpg");
+            TextureLoader texLoader =  new TextureLoader( ur, this);
+            texEarth = texLoader.getTexture();
+            
+            
+            //texLoader =  new TextureLoader( "SatCtrl/moon___map_by_horizoied-d3y3lvg.jpg", this);
+            ur = new URL("http://24.84.57.253/SatCtrl/moon___map_by_horizoied-d3y3lvg.jpg");
+            texLoader =  new TextureLoader( ur, this);
+            texMoon = texLoader.getTexture();
+        
+            //URL urlJpg = new URL("http://24.84.57.253/SatCtrl/071320112967.jpg");
             //InputStream fJpgFile = urlJpg.openStream();
             //TextureLoader texLoader =  new TextureLoader( fJpgFile, "RGBA", this);//new Container());//this );
             
             
             //TextureLoader texLoader =  new TextureLoader( "SatCtrl/Earth-Color_960_Koord.jpg", "RGBA", this);//new Container());//this );
         
-            //TextureLoader texLoader =  new TextureLoader( "Map_Earth_2100_by_JamesVF.jpg","RGB", this);
+            //URL urlJpg = new URL("http://24.84.57.253/SatCtrl/Earth-Color_960_Koord.jpg");
+            //InputStream fJpgFile = urlJpg.openStream();
+            //TextureLoader texLoader =  new TextureLoader( "SatCtrl/071320112967.jpg","RGB", this);
+            //ImageComponent2D image = texLoader.getImage();
+            //texEarth = new Texture2D();
+            //texEarth.setImage(0, image);
             //URL myURl = URL("http://192.168.0.102/SatCtrl/Map_Earth_2100_by_JamesVF.jpg");
-            //texEarth = new TextureLoader("http://192.168.0.102/SatCtrl/Map_Earth_2100_by_JamesVF.jpg", this).getTexture();
+            //texEarth = new TextureLoader("SatCtrl/071320112967.jpg", this).getTexture();
             //ImageComponent myImage = texLoader.getImage();
             //texEarth = new Texture2D();
             //texEarth.setImage(0, myImage);
-            //texEarth.setBoundaryModeS(Texture.CLAMP);//.WRAP);
-            //texEarth.setBoundaryModeT(Texture.CLAMP);//.WRAP);
+            
+           //texEarth.
+            //texEarth.setBoundaryModeS(Texture.RGB);
+            //texEarth.setBoundaryModeT(Texture.RGB);
+            
             //texEarth.setImage(WIDTH, null);
             //texEarth.setBoundaryColor(new Color4f(0.0f, 1.0f, .0f, 1.0f));
         
@@ -357,8 +447,11 @@ public class visualtra extends Applet {
         GraphicsConfiguration config =
            SimpleUniverse.getPreferredConfiguration();
 
+        //Canvas3D c = new Canvas3D(config);
         Canvas3D c = new Canvas3D(config);
+        c.setSize(100, 100);
 	add("Center", c);
+        //add("Top", c);
 
 	u = new SimpleUniverse(c);
 	BranchGroup scene = createSceneGraph(u);
@@ -366,6 +459,10 @@ public class visualtra extends Applet {
         // This will move the ViewPlatform back a bit so the
         // objects in the scene can be viewed.
         u.getViewingPlatform().setNominalViewingTransform();
+        
+        //u.getViewingPlatform().setPlatformGeometry(null);
+        
+        
 
 	u.addBranchGraph(scene);
     }
